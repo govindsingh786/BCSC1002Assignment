08199510a425b476dd4556e91016b38ae16128fb
Govind Singh <govin.singh_cs19@gla.ac.in>
2020-08-29 21:17:24 +0530

    getter method and setter method was executed successfully.

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -13,6 +13,11 @@ import definitions.Book;
 public class FrontDesk {
     public static void main(String[] args) {
         Book book = new Book();
+        book.setBookName("The Girl Next Door");
+        book.setAuthorName("Jack Ketchum");
+        book.setBookISBNDigitNumber("99921-58-10-7");
+        System.out.println("Book Name is : " + book.getBookName() + "," + "Author Name is : " + book.getAuthorName()
+                + "," + "ISBN NO : " + book.getBookISBNDigitNumber());
 
 
     }
