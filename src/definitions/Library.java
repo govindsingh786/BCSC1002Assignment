d59a3b07b8920bb79e4193bd54989a43631df49c
Govind Singh <govin.singh_cs19@gla.ac.in>
2020-08-29 17:04:22 +0530

    create a Library.java for definitions package

---
+++ src/definitions/Library.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Govind Singh
+ *   Date: 29-08-2020
+ *   Time: 17:03
+ *   File: Library.java
+ */
+
+package definitions;
+
+public class Library {
+}
