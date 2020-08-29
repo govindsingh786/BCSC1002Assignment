c469b8ccecd9c9712bd936be09061075f0406c6c
Govind Singh <govin.singh_cs19@gla.ac.in>
2020-08-29 17:24:07 +0530

    create a FrontDesk.java in execution package

---
+++ src/execution/FrontDesk.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Govind Singh
+ *   Date: 29-08-2020
+ *   Time: 17:22
+ *   File: FrontDesk.java
+ */
+
+package execution;
+
+public class FrontDesk {
+}
