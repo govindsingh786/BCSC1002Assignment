bfcc94132e5be393ec03486286664aa10465d8d0
Govind Singh <govin.singh_cs19@gla.ac.in>
2020-08-29 21:48:20 +0530

    equals() and the hashCode() method was executed successfully.

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -13,11 +13,16 @@ import definitions.Book;
 public class FrontDesk {
     public static void main(String[] args) {
         Book book = new Book("Blood Meridian", " Cormac McCarthy", "1 86197 271-7");
+        Book book1 = new Book("Exquisite Corpse", "Poppy Z. Brite", "2 46287 731-4");
         //book.setBookName("The Girl Next Door");
         //book.setAuthorName("Jack Ketchum");
         //book.setBookISBNDigitNumber("99921-58-10-7");
-
-        System.out.println(book);
+        //System.out.println(book);
+        if (book.equals(book1)) {
+            System.out.println("equal");
+        } else {
+            System.out.println("Not equal");
+        }
 
 
     }
