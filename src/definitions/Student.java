dc40ded7874878a1b5ba8fd3c942fe0329cf7204
Govind Singh <govin.singh_cs19@gla.ac.in>
2020-08-29 15:04:19 +0530

    add Student.java class in definitions package

---
+++ src/definitions/Student.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Govind Singh
+ *   Date: 29-08-2020
+ *   Time: 15:03
+ *   File: Student.java
+ */
+
+package definitions;
+
+public class Student {
+}
